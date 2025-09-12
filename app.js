6f38dad9814293c2b6e25c3efd354762e6560f9e
Alexander Strekanov <strekanov.alexander@gmail.com>
2025-09-12 20:33:48 +0200

    rebuilded: with start app.js without db

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Backend is running!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+/*
 import cors from 'cors'
 import express from 'express';
 import sequelize from './src/config/db.js';
@@ -30,3 +49,4 @@ try {
 } catch (err) {
     console.error('Failed to start server:', err);
 }
+    */
