0c825ca8307c81e154398d942256951f5380fd82
Alexander Strekanov <strekanov.alexander@gmail.com>
2025-09-12 20:20:55 +0200

    rebuilded: with cors settings

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ import userRoutes from './src/routes/userRoutes.js';
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+// app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
