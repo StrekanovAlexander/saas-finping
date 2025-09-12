380d52151c691b5c422f1e7f8cad99b1c75fb1ef
Alexander Strekanov <strekanov.alexander@gmail.com>
2025-09-12 22:47:49 +0200

    rebuilded: app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import cors from 'cors'
 import express from 'express';
-import { getSequelize } from "./src/config/db.js";
 
-// import sequelize from './src/config/db.js';
+// import { getSequelize } from "./src/config/db.js";
+import sequelize from './src/config/db.js';
+
 // import assetRoutes from './src/routes/assetRoutes.js';
 // import trackingRoutes from './src/routes/trackingRoutes.js';
 // import userRoutes from './src/routes/userRoutes.js';
+
 // import './src/cron/checkTrackings.js';
 // import './src/cron/updateAssets.js';
 const app = express();
@@ -15,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const sequelize = getSequelize();
+// const sequelize = getSequelize();
 
 app.get('/', (req, res) => {
   res.json({ message: 'Backend is running!' });
@@ -24,21 +26,23 @@ app.get('/', (req, res) => {
 app.get("/test-db", async (req, res) => {
   try {
     await sequelize.authenticate();
-    res.json({ success: true, message: "Database connected!!!" });
+    res.json({ success: true, message: "Database is connected!" });
   } catch (err) {
     res.status(500).json({ success: false, error: err.message });
   }
 });
 
-// app.use('/api/assets', assetRoutes);
-// app.use('/api/trackings', trackingRoutes);
-// app.use('/api/users', userRoutes);
+/*
+app.use('/api/assets', assetRoutes);
+app.use('/api/trackings', trackingRoutes);
+app.use('/api/users', userRoutes);
+*/
 
 const PORT = process.env.PORT || 3000;
 
 try {
     // await sequelize.sync();
-    await sequelize.authenticate();
+    // await sequelize.authenticate();
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
--- src/config/db.js
+++ src/config/db.js
@@ -1,27 +1,26 @@
-import { Sequelize } from "sequelize";
-import dotenv from 'dotenv';
-dotenv.config();
+// import { Sequelize } from "sequelize";
+// import dotenv from 'dotenv';
+// dotenv.config();
+// 
+// let sequelize;
+// 
+// export function getSequelize() {
+//     if (!sequelize) {
+//         sequelize = new Sequelize(
+//         process.env.DB_NAME,
+//         process.env.DB_USER,
+//         process.env.DB_PASSWORD,
+//         {
+//             host: process.env.HOST,
+//             port: process.env.DB_PORT || 3306,
+//             dialect: "mysql",
+//             logging: false,
+//         }
+//         );
+//     }
+//     return sequelize;
+// }
 
-let sequelize;
-
-export function getSequelize() {
-    if (!sequelize) {
-        sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: process.env.HOST,
-            port: process.env.DB_PORT || 3306,
-            dialect: "mysql",
-            logging: false,
-        }
-        );
-    }
-    return sequelize;
-}
-
-/*
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
@@ -31,10 +30,11 @@ const sequelize = new Sequelize(
     process.env.DB_NAME, 
     process.env.DB_USER, 
     process.env.DB_PASSWORD, {
-    host: process.env.HOST,
-    dialect: 'mysql',
-    logging: false
-});
+        host: process.env.HOST,
+        port: process.env.DB_PORT || 3306,
+        dialect: "mysql",
+        logging: false,
+    });
 
 try {
     await sequelize.authenticate();
@@ -44,4 +44,3 @@ try {
 }
 
 export default sequelize;
-*/
