ce2497a2aa13a15b0985adcf80d46cc5dcacfb55
Alexander Strekanov <strekanov.alexander@gmail.com>
2025-09-14 20:31:12 +0200

    added: cron testing functional

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ import express from 'express';
 import sequelize from './src/config/db.js';
 import { assetRoutes, trackingRoutes, userRoutes } from './src/routes/index.js';
 // import './src/cron/checkTrackings.js';
-import './src/cron/updateAssets.js';
+// import './src/cron/updateAssets.js';
+
+import { updateAssetPrices } from './src/services/priceUpdater.js';
+
 const app = express();
 app.use(cors()); // app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
@@ -15,6 +18,7 @@ app.get('/', (req, res) => {
 
 app.get("/test-db", async (req, res) => {
   try {
+    await updateAssetPrices();
     res.json({ success: true, message: "Database is connected!" });
   } catch (err) {
     res.status(500).json({ success: false, error: err.message });
