7b2b8413d1a0b83328e1ea3c941eaba041348d49
Alexander Strekanov <strekanov.alexander@gmail.com>
2025-09-16 18:09:58 +0200

    rebuilded: with app.js

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// runUpdateAssetPrices();
-// setInterval(runUpdateAssetPrices, 15 * 60 * 1000);
+// runUpdateAssetPrices(); setInterval(runUpdateAssetPrices, 15 * 60 * 1000);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Backend is running!' });
