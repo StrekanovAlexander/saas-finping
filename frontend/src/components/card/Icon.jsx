199ec482b09dbfcf93cd4532139293c4a6b54d00
Alexander Strekanov <strekanov.alexander@gmail.com>
2025-09-20 23:57:31 +0200

    added: icons

--- frontend/src/components/card/Icon.jsx
+++ frontend/src/components/card/Icon.jsx
@@ -1,9 +1,11 @@
 import * as Icons from "lucide-react";
 
 const icons = {
+    Bean: Icons.Bean,
     Bitcoin: Icons.Bitcoin,
     CircleDot: Icons.CircleDot,
     Coins: Icons.Coins,
+    DollarSign: Icons.DollarSign,
     Droplet: Icons.Droplet,
     Euro: Icons.Euro, 
     JapaneseYen: Icons.JapaneseYen,
@@ -11,6 +13,8 @@ const icons = {
     Leaf: Icons.Leaf,
     Package: Icons.Package,
     PoundSterling: Icons.PoundSterling, 
+    SwissFranc: Icons.SwissFranc,
+    Wheat: Icons.Wheat
 };
 
 function Icon({ icon }) {
